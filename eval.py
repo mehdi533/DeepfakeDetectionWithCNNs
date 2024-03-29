d745ece3a2f416c52d29dc2dc1aec17b4f29f53d
mabda533 <mabda533@gmail.com>
2024-03-30 00:28:30 +0100

    Add first code

---
+++ eval.py
@@ -0,0 +1,64 @@
+import os
+import csv
+import torch
+
+from validate import validate
+from networks.resnet import resnet50
+#-----------–-----------–-----------–--------
+import torchvision.models as models
+import torch.nn as nn
+from networks.custom_models import *
+#-----------–-----------–-----------–--------
+from options.test_options import TestOptions
+from eval_config import *
+
+
+def evaluation(model_path, name, opt):
+    # Running tests
+    model_name = os.path.basename(model_path).replace('.pth', '')
+    rows = [["{} model testing on...".format(name)],
+            ['testset', 'accuracy', 'avg precision', "f1 score", "roc score", "recall", "precision"]]
+
+    print("{} model testing on...".format(name))
+
+    # ---------------------------------------------------------------------
+    opt.models = ["real", "PNDM", "DDPM", "LDM", "ProGAN"]
+    list_models = opt.models
+    list_models.remove("real")
+
+    for v_id, test_model in enumerate(list_models):
+            
+        #opt.dataroot = '{}/{}'.format(dataroot, val)
+        #opt.classes = os.listdir(opt.dataroot) if multiclass[v_id] else ['']
+        #opt.classes = [0, 1]
+    # ---------------------------------------------------------------------
+        opt.no_resize = True    # testing without resizing by default
+
+        # model = resnet50(num_classes=1)
+        # model = models.efficientnet_b0(num_classes=1)
+        # num_ftrs = model.classifier[1].in_features
+        # model.classifier[1] = nn.Linear(num_ftrs, 1)
+        # model.fc = nn.Linear(2048, 1)
+        # model = models.vgg16(num_classes=1)
+        model = VGG16(num_classes=1)
+        
+        state_dict = torch.load(model_path, map_location='cpu')
+        model.load_state_dict(state_dict['model'])
+        model.cuda()
+        model.eval()
+        opt.models = [test_model, "real"]
+        # ---------------------------------------------------------------------
+        acc, ap, r_acc, f_acc, f1, auc, prec, recall, _, _ = validate(model, opt, "test_list")
+        rows.append([test_model, acc, ap, f1, auc, prec, recall])
+        print("({}) acc: {}; ap: {}; r_acc: {}; f_acc: {} f1: {}; roc_auc: {}; recall: {}; precision: {}".format(test_model, acc, ap, r_acc, f_acc, f1, auc, recall, prec))
+        # ---------------------------------------------------------------------
+
+    csv_name = results_dir + '/{}.csv'.format(name)
+    with open(csv_name, 'w') as f:
+        csv_writer = csv.writer(f, delimiter=',')
+        csv_writer.writerows(rows)
+
+
+if __name__ == "__main__":
+    opt = TestOptions().parse(print_options=False)
+    evaluation(opt.model_path, opt.name, opt)
---
+++ validate.py
@@ -0,0 +1,56 @@
+import torch
+import numpy as np
+from networks.resnet import resnet50
+from sklearn.metrics import average_precision_score, precision_recall_curve, accuracy_score, f1_score, roc_auc_score, precision_score, recall_score
+from options.test_options import TestOptions
+from data import create_dataloader
+
+
+def validate(model, opt, __type):
+    # --------------------------------------------------------------------------------
+    data_loader = create_dataloader(opt, __type)
+    # --------------------------------------------------------------------------------
+    with torch.no_grad():
+        y_true, y_pred = [], []
+        for img, label in data_loader:
+            try:
+                in_tens = img.cuda()
+            except AssertionError:
+                in_tens = img
+            y_pred.extend(model(in_tens).sigmoid().flatten().tolist())
+            y_true.extend(label.flatten().tolist())
+
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+
+    # ---------------------------------------- ----------------------------------------
+    r_acc = accuracy_score(y_true[y_true==0], y_pred[y_true==0] > 0.5)
+    f_acc = accuracy_score(y_true[y_true==1], y_pred[y_true==1] > 0.5)
+    acc = accuracy_score(y_true, y_pred > 0.5)
+    ap = average_precision_score(y_true, y_pred)
+    f1score = f1_score(y_true, y_pred > 0.5, average='binary')
+    prec = precision_score(y_true, y_pred > 0.5)
+    recall = recall_score(y_true, y_pred > 0.5)
+    auc_score = roc_auc_score(y_true, y_pred)
+    # ---------------------------------------- ----------------------------------------
+    return acc, ap, r_acc, f_acc, f1score, auc_score, prec, recall, y_true, y_pred
+
+
+if __name__ == '__main__':
+    opt = TestOptions().parse(print_options=False)
+
+    model = resnet50(num_classes=1)
+    state_dict = torch.load(opt.model_path, map_location='cpu')
+    model.load_state_dict(state_dict['model'])
+
+    try:
+        model.cuda()
+    finally:
+        model.eval()
+
+    acc, avg_precision, r_acc, f_acc, y_true, y_pred = validate(model, opt)
+
+    print("accuracy:", acc)
+    print("average precision:", avg_precision)
+
+    print("accuracy of real images:", r_acc)
+    print("accuracy of fake images:", f_acc)
